646a75db3e223fe94907c15bbe0eb1647f0e91e7
Samuel Tseng <samuel.tseng@homeday.de>
2024-01-31 16:28:24 +0100

    multiple guess with loop

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,30 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
-    // --snip--
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
     
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
+    loop {
 
-    let mut guess = String::new();
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
 }
